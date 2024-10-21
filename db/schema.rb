077c4432f8c6b5e287d4590d516cba83091e6051
David M <davidmatamoros90@gmail.com>
2024-10-20 20:11:47 -0600

    rails generate model Status

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2024_10_21_021045) do
+ActiveRecord::Schema[8.1].define(version: 2024_10_21_021129) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -31,6 +31,15 @@ ActiveRecord::Schema[8.1].define(version: 2024_10_21_021045) do
     t.index ["user_id"], name: "index_sessions_on_user_id"
   end
 
+  create_table "statuses", force: :cascade do |t|
+    t.string "name"
+    t.integer "position"
+    t.bigint "project_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["project_id"], name: "index_statuses_on_project_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email_address", null: false
     t.string "password_digest", null: false
@@ -41,4 +50,5 @@ ActiveRecord::Schema[8.1].define(version: 2024_10_21_021045) do
 
   add_foreign_key "projects", "users"
   add_foreign_key "sessions", "users"
+  add_foreign_key "statuses", "projects"
 end
