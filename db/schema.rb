d78293d3fee8ec72426be5eb6a14f2552a1b1db5
David M <davidmatamoros90@gmail.com>
2024-10-20 20:13:14 -0600

    rails generate model Task

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2024_10_21_021129) do
+ActiveRecord::Schema[8.1].define(version: 2024_10_21_021302) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -40,6 +40,17 @@ ActiveRecord::Schema[8.1].define(version: 2024_10_21_021129) do
     t.index ["project_id"], name: "index_statuses_on_project_id"
   end
 
+  create_table "tasks", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.bigint "project_id", null: false
+    t.bigint "status_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["project_id"], name: "index_tasks_on_project_id"
+    t.index ["status_id"], name: "index_tasks_on_status_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email_address", null: false
     t.string "password_digest", null: false
@@ -51,4 +62,6 @@ ActiveRecord::Schema[8.1].define(version: 2024_10_21_021129) do
   add_foreign_key "projects", "users"
   add_foreign_key "sessions", "users"
   add_foreign_key "statuses", "projects"
+  add_foreign_key "tasks", "projects"
+  add_foreign_key "tasks", "statuses"
 end
